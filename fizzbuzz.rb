f7b5901751386cafbd075cdb25dc72e5a35343d0
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 14:02:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 require 'pry'
 
 def fizzbuzz(int)
- if int % 3 == 0
+if int % 3 == 0 && 5 == 0
+  "FizzBuzz"
+elsif int % 3 == 0
    "Fizz"
  elsif int % 5 == 0
    "Buzz"
- elsif int % 3 == 0 && 5 == 0
-   "FizzBuzz"
  else nil
  end
 end
