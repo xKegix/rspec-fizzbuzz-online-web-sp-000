458e87363f66050f90463f5700e2ca1fc54fb96a
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 13:52:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative '../fizzbuzz.rb'
 require 'pry'
 
 def fizzbuzz(int)
