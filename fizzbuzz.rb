0d112ae8dd430586c500f54304cca6379f4b7ad7
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 13:51:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative '../fizzbuzz.rb'
+require_relative './fizzbuzz.rb'
 require 'pry'
 
 def fizzbuzz(binding.pry)
